7594c8032e02fda5ec2510ca71c4ad3b1b908db2
StaceyS27 <131292486+StaceyS27@users.noreply.github.com>
2023-08-24 17:59:38 -0400

    Add files via upload

---
+++ prescriptions.test.js
@@ -0,0 +1,110 @@
+const { getTotalCost, applyDiscount, applyCoupon } = require("../index.js");
+
+// NOTE: This array illustrates the shape of the data that
+// will be used in the functions you are testing.
+const sampleData = [
+  {
+    prescription: "acetaminophen",
+    pricePerRefill: 25,
+    refills: 3,
+    subscription: false,
+    coupon: true,
+  },
+  {
+    prescription: "diphenhydramine",
+    pricePerRefill: 50,
+    refills: 1,
+    subscription: true,
+    coupon: false,
+  },
+  {
+    prescription: "phenylephrine",
+    pricePerRefill: 30,
+    refills: 5,
+    subscription: true,
+    coupon: true,
+  },
+];
+
+// NOTE: You do not need to modify this describe block; it is provided for you.
+// These tests are not exhaustive, but they should be enough to give you
+// an idea of what you might want to test, and how to do so.
+describe("getTotalCost()", () => {
+  describe("handles expected input", () => {
+    it("returns the correct cost for a single refill", () => {
+      expect(getTotalCost(25, 1)).toBe(25);
+    });
+    it("returns the correct cost for multiple refills", () => {
+      expect(getTotalCost(25, 3)).toBe(75);
+    });
+    it("returns the correct cost for a zero-cost refill", () => {
+      expect(getTotalCost(0, 3)).toBe(0);
+    });
+    it("returns the correct cost for zero refills", () => {
+      expect(getTotalCost(25, 0)).toBe(0);
+    });
+    it("returns the correct cost for random numbers", () => {
+      const pricePerRefill = Math.floor(Math.random() * 100);
+      const refills = Math.floor(Math.random() * 10);
+      const result = getTotalCost(pricePerRefill, refills);
+      const expected = pricePerRefill * refills;
+      expect(result).toBe(expected);
+    });
+    it("returns the correct costs for the sample data", () => {
+      sampleData.forEach((data) => {
+        const result = getTotalCost(data.pricePerRefill, data.refills);
+        const expected = data.pricePerRefill * data.refills;
+        expect(result).toBe(expected);
+      });
+    });
+  });
+
+  // NOTE: These tests will fail - why?
+  describe.skip("handles unexpected input", () => {
+    it("returns 0 if either argument is not a number", () => {
+      expect(getTotalCost("25", 3)).toBe(0);
+      expect(getTotalCost(25, "3")).toBe(0);
+      expect(getTotalCost("25", "3")).toBe(0);
+    });
+    it("returns 0 if pricePerRefill is negative", () => {
+      expect(getTotalCost(-25, 3)).toBe(0);
+    });
+    it("returns 0 if refills is negative", () => {
+      expect(getTotalCost(25, -3)).toBe(0);
+    });
+  });
+});
+
+// TODO: Write tests for applyDiscount() and applyCoupon()
+
+describe("applyDiscount", function() {
+  it("should be a function", function () {
+    expect(typeof applyDiscount).toBe('function')
+  })
+
+  it("should provide same price w/ no subscription", function(){
+    result = applyDiscount(100, false);
+    expect(result).toBe(100)
+  })
+
+  it.only("should result in a 25% discount", function() {
+    result = applyDiscount(100, true);
+    expect(result).toBe(75)
+  })
+})
+
+describe("applyCoupon", function () {
+  it("should be a function", function() {
+    expect(typeof applyCoupon).toBe('function')
+  })
+
+  it("should return discount of 10 if coupon present", function () {
+    result = applyCoupon(100, true);
+    expect(result).toBe(90)
+  })
+
+  it.only("should return same price if no discount present", function () {
+    result = applyCoupon(100, false);
+    expect(result).toBe(100)
+  })
+})
